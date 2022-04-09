045bf3ba548df241cb26b4504c1b6200f1bb3d4c
mysend12 <mysend12@naver.com>
2022-04-09 17:05:11 +0900

    JWT 토큰 만료 에러 예외처리

--- src/main/java/io/my/base/util/JwtUtil.java
+++ src/main/java/io/my/base/util/JwtUtil.java
@@ -1,9 +1,6 @@
 package io.my.base.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.*;
 import io.my.base.properties.jwt.AccessTokenProperties;
 import io.my.base.properties.jwt.RefreshTokenProperties;
 import lombok.RequiredArgsConstructor;
@@ -97,7 +94,7 @@ public class JwtUtil {
                     .setSigningKey(key)
                     .parseClaimsJws(jwt)
                     .getBody();
-        } catch(SignatureException e) {
+        } catch(SignatureException | ExpiredJwtException e) {
             return null;
         }
     }
