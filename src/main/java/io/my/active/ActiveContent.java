0441280673f26b9c50cc0c241b30c35f253776d1
mysend12 <mysend12@naver.com>
2022-07-31 13:13:01 +0900

    활동 내역에서 이름 부분에 태그 추가

--- src/main/java/io/my/active/ActiveContent.java
+++ src/main/java/io/my/active/ActiveContent.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum ActiveContent {
-    ADDED_FRIEND("{}님을 친구로 추가하였습니다."),
-    BE_ADDED_FRIEND("{}님이 회원님을 친구로 추가하였습니다."),
-    DELETED_FRIEND("{}님을 친구 목록에서 삭제하였습니다.")
+    ADDED_FRIEND("<b>{}</b>님을 친구로 추가하였습니다."),
+    BE_ADDED_FRIEND("<b>{}</b>님이 회원님을 친구로 추가하였습니다."),
+    DELETED_FRIEND("<b>{}</b>님을 친구 목록에서 삭제하였습니다.")
     ;
 
     private String content;
